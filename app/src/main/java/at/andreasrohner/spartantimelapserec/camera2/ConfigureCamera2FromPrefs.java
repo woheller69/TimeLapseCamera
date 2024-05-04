82c49f2ed3e15265cf0506c92cac0ab5820adb3a
Andreas Butti <andreas.butti@gmail.com>
2024-05-04 15:40:19 +0200

    Use autofocus settings

--- app/src/main/java/at/andreasrohner/spartantimelapserec/camera2/ConfigureCamera2FromPrefs.java
+++ app/src/main/java/at/andreasrohner/spartantimelapserec/camera2/ConfigureCamera2FromPrefs.java
@@ -2,11 +2,20 @@ package at.andreasrohner.spartantimelapserec.camera2;
 
 import android.content.SharedPreferences;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.MeteringRectangle;
+import android.util.Log;
+
+import at.andreasrohner.spartantimelapserec.rest.HttpThread;
 
 /**
  * Configure camera from android preferences
  */
 public class ConfigureCamera2FromPrefs {
+
+	/**
+	 * Log Tag
+	 */
+	private static final String TAG = HttpThread.class.getSimpleName();
 
 	/**
 	 * Preferences
@@ -42,8 +51,46 @@ public class ConfigureCamera2FromPrefs {
 		String afMode = prefs.getString("pref_camera_af_mode", null);
 
 		if ("field".equals(afMode)) {
-			// TODO !!!!!!!!!!!!!!!
-			//			captureBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_MACRO);
+			// TODO This value is dependent on the current image resolution!
+
+			String afField = prefs.getString("pref_camera_af_field", null);
+			if (afField == null) {
+				Log.e(TAG, "Missing AF Field!");
+				return;
+			}
+
+			afField = afField.substring(1, afField.length() - 2);
+			String[] parts = afField.split(",");
+			int x = 0;
+			int y = 0;
+			int width = 1;
+			int height = 1;
+			int meteringWeight = 1;
+			for (String p : parts) {
+				p = p.trim();
+				Log.e("xxxxxxxx", "xxxxx " + p);
+
+				String[] f = p.split(":");
+				if (f.length != 2) {
+					continue;
+				}
+
+				int val = Integer.parseInt(f[1]);
+				if ("x".equals(f[0])) {
+					x = val;
+				} else if ("y".equals(f[0])) {
+					y = val;
+				} else if ("w".equals(f[0])) {
+					width = val;
+				} else if ("h".equals(f[0])) {
+					height = val;
+				} else if ("wt".equals(f[0])) {
+					meteringWeight = val;
+				}
+			}
+
+			MeteringRectangle focusAreaTouch = new MeteringRectangle(x, y, width, height, meteringWeight);
+			captureBuilder.set(CaptureRequest.CONTROL_AF_REGIONS, new MeteringRectangle[] {focusAreaTouch});
 		} else if ("manual".equals(afMode)) {
 			captureBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_OFF);
 			float focusDistance = prefs.getFloat("pref_camera_af_manual", 0);
